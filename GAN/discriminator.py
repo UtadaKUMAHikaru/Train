ac9624d940f81ca353892a3b37486dc31f3380a8
hirosue <12281163@bjtu.edu.cn>
2021-11-15 21:54:22 +0800

    first commit

---
+++ GAN/discriminator.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+import numpy as np
+
+latent_dim = 100
+img_shape = (1, 128, 128)
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+
+        return validity
